647c14447c53be0fd1a5f19bddd9c816c9ba7aa0
eudesf <eudesf@remanso.biz>
2013-06-27 17:34:02 -0300

    Adicionado teste de atualização do KnowledgeBase pelo KnowledgeAgent

--- src/main/java/com/redhat/brmsworkshop/Main.java
+++ src/main/java/com/redhat/brmsworkshop/Main.java
@@ -1,11 +1,11 @@
 package com.redhat.brmsworkshop;
 
-import java.io.IOException;
-
 import org.drools.KnowledgeBase;
 import org.drools.agent.KnowledgeAgent;
 import org.drools.agent.KnowledgeAgentFactory;
 import org.drools.definition.type.FactType;
+import org.drools.io.ResourceChangeScannerConfiguration;
+import org.drools.io.ResourceFactory;
 import org.drools.io.impl.ClassPathResource;
 import org.drools.runtime.StatefulKnowledgeSession;
 
@@ -16,10 +16,17 @@ public class Main {
 	}
 
 	private void launch() throws Exception {
+		ResourceChangeScannerConfiguration configuration = ResourceFactory.getResourceChangeScannerService().newResourceChangeScannerConfiguration();
+		configuration.setProperty("drools.resource.scanner.interval", "10");
+		ResourceFactory.getResourceChangeScannerService().configure(configuration);
+		
 		KnowledgeAgent knowledgeAgent = KnowledgeAgentFactory.newKnowledgeAgent("MyAgent");
 		knowledgeAgent.applyChangeSet(new ClassPathResource("changeset.xml"));
 		KnowledgeBase knowledgeBase = knowledgeAgent.getKnowledgeBase();
 		StatefulKnowledgeSession ksession = knowledgeBase.newStatefulKnowledgeSession();
+
+		ResourceFactory.getResourceChangeNotifierService().start();
+		ResourceFactory.getResourceChangeScannerService().start();
 		
 		FactType factType = knowledgeBase.getFactType("cleartech", "Customer");
 		Object fact = factType.newInstance();
@@ -28,8 +35,18 @@ public class Main {
 		ksession.insert(fact);
 		ksession.fireAllRules();
 		System.out.println("approved? -> " + factType.get(fact, "approved"));
+		
+		Thread.sleep(60000);
+		
+		Object fact2 = factType.newInstance();
+		factType.set(fact2, "age", 28);
+		
+		knowledgeBase = knowledgeAgent.getKnowledgeBase();
+		ksession = knowledgeBase.newStatefulKnowledgeSession();
+		ksession.insert(fact2);
+		ksession.fireAllRules();
+		System.out.println("aprovado? -> " + factType.get(fact2, "approved") );
 	}
-	
 	
 
 }
